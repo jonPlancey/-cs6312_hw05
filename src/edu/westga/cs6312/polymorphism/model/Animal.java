3f7f0321e5359b6b7f576bedf16fe40ebff31380
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-10 18:14:57 -0500

    + Animal constructor with preconditons added

--- src/edu/westga/cs6312/polymorphism/model/Animal.java
+++ src/edu/westga/cs6312/polymorphism/model/Animal.java
@@ -1,6 +1,7 @@
 package edu.westga.cs6312.polymorphism.model;
 
 
+
 /**
  * Homework 
  * @author 	cs6312
@@ -12,5 +13,26 @@ public abstract class Animal {
 	private String kind;
 	private String covering;
 
+	/** 
+	 * 2 parameter constructor to create an Animal object 
+	 * @param animalkind 		The kind of animal being created 
+	 * @param animalCovering 	The animal's covering type 
+	 * 
+	 * Precondition: kind != null 
+ 	 * covering:	 != null 
+	 * Postcondition: An animal of whose type is kind and is
+	 * covered with covering
+	 */ 
+	public Animal(String animalkind, String animalCovering) {
+		if (animalkind == null) {
+			throw new IllegalArgumentException("Invalid kind");
+		}	
+		if (animalCovering == null) {
+			throw new IllegalArgumentException("Invalid covering"); 					
+		}
+		this.kind = animalkind;
+		this.covering = animalCovering; 	
+	} 
 	
+
 }
