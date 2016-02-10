fd886e6ad618b9d35388b6981df5a80162f30bdb
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-10 17:57:03 -0500

    + instance variables, kind covering added

--- src/edu/westga/cs6312/polymorphism/model/Animal.java
+++ src/edu/westga/cs6312/polymorphism/model/Animal.java
@@ -1,5 +1,16 @@
 package edu.westga.cs6312.polymorphism.model;
 
-public abstract class Animal {
 
+/**
+ * Homework 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.10.2016
+ */
+public abstract class Animal {
+	
+	private String kind;
+	private String covering;
+
+	
 }
