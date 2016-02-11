0ef1805669a7fe1bb72021144406018bc9dc4f49
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-11 03:31:57 -0500

    + crow, cat & dog tests in AnimalWhenGetNewAnimal added

--- src/edu/westga/cs6312/polymorphism/testing/AnimalWhenGetNewAnimal.java
+++ src/edu/westga/cs6312/polymorphism/testing/AnimalWhenGetNewAnimal.java
@@ -3,6 +3,9 @@ package edu.westga.cs6312.polymorphism.testing;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import edu.westga.cs6312.polymorphism.model.BirdEagle;
+import edu.westga.cs6312.polymorphism.model.BirdCrow;
+import edu.westga.cs6312.polymorphism.model.MammalCat;
+import edu.westga.cs6312.polymorphism.model.MammalDog;
 
 
 
@@ -16,10 +19,9 @@ public class AnimalWhenGetNewAnimal {
 
 	
 	/** 
-	 * Test that getNewAnimal return proper animal
+	 * BirdEagle test: getNewAnimal return proper animal detailes
 	 */	
 	@Test
-	//testBallWithColorYellow
 	public void testAnimalWhenGetNewAnimalEagle() {	
 		BirdEagle eagle = new BirdEagle();
 		String animal = BirdEagle.getNewAnimal("eagle");
@@ -28,5 +30,50 @@ public class AnimalWhenGetNewAnimal {
 		String results = eagle.toString();
 		assertEquals(expect, results);
 	}
+	
+
+	
+	/** 
+	 * BirdEagle test: getNewAnimal return proper animal detailes
+	 */	
+	@Test
+	public void testAnimalWhenGetNewAnimalCrow() {	
+		BirdCrow crow = new BirdCrow();
+		String animal = BirdEagle.getNewAnimal("crow");
+		
+		String expect = "Animal kind: " + animal + ", covering: feathers"; 
+		String results = crow.toString();
+		assertEquals(expect, results);
+	}
+	
+
+	
+	/** 
+	 * BirdEagle test: getNewAnimal return proper animal detailes
+	 */	
+	@Test
+	public void testAnimalWhenGetNewAnimalCat() {	
+		MammalCat cat = new MammalCat();
+		String animal = BirdEagle.getNewAnimal("eagle");
+		
+		String expect = "Animal kind: " + animal + ", covering: feathers"; 
+		String results = cat.toString();
+		assertEquals(expect, results);
+	}
+	
+
+	
+	/** 
+	 * BirdEagle test: getNewAnimal return proper animal detailes
+	 */	
+	@Test
+	public void testAnimalWhenGetNewAnimalDog() {	
+		MammalDog dog = new MammalDog();
+		String animal = BirdEagle.getNewAnimal("eagle");
+		
+		String expect = "Animal kind: " + animal + ", covering: feathers"; 
+		String results = dog.toString();
+		assertEquals(expect, results);
+	}	
 
 }
