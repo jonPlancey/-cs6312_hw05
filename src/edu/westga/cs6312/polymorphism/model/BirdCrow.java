fb074374236147d11052dc789ed2f1b7f7a46ded
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-11 00:39:30 -0500

    + BirdCrow added to BirdCrow

--- src/edu/westga/cs6312/polymorphism/model/BirdCrow.java
+++ src/edu/westga/cs6312/polymorphism/model/BirdCrow.java
@@ -1,5 +1,36 @@
 package edu.westga.cs6312.polymorphism.model;
 
-public class BirdCrow {
 
+
+/**
+ * Homework 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.10.2016
+ */
+public class BirdCrow extends Bird {
+	/** 
+	 * constructor to create an BirdCrow object 
+	 * 
+	 */ 
+	public BirdCrow() {
+		super("crow");
+	}
+	
+	/** 
+	 * Gets the sound the animal makes
+	 * @return returns the sound of animal
+	 */ 	
+	@Override
+	public String getSound() {
+		return super.getSound();
+	};	
+	
+	/** 
+	 * Sets the sound of animal 
+	 * @param sound  sound animal makes 
+	 */ 	
+	public void setSound(String sound) { 	
+		return super.setSound();
+	};	
 }
--- src/edu/westga/cs6312/polymorphism/model/MammalCat.java
+++ src/edu/westga/cs6312/polymorphism/model/MammalCat.java
@@ -1,5 +1,13 @@
 package edu.westga.cs6312.polymorphism.model;
 
+
+
+/**
+ * Homework 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.10.2016
+ */
 public class MammalCat {
 
 }
