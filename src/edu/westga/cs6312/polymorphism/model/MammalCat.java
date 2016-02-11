889b8db78ac3f2ceb6dd53eab7e0f05079bd08ea
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-11 00:58:48 -0500

    # MammalCat, defined cat and gave it a sound

--- src/edu/westga/cs6312/polymorphism/model/MammalCat.java
+++ src/edu/westga/cs6312/polymorphism/model/MammalCat.java
@@ -8,6 +8,23 @@ package edu.westga.cs6312.polymorphism.model;
  * @author 	jim perry, jr.
  * @version	02.10.2016
  */
-public class MammalCat {
+public class MammalCat extends Bird {
+	
+	/** 
+	 * constructor to create an BirdCrow object 
+	 * 
+	 */ 
+	public MammalCat() {
+		super("cat");
+	}
+	
+	/** 
+	 * Gets the sound the animal makes
+	 * @return returns the sound of animal
+	 */ 	
+
+	public String getSound() {
+		return "meow";
+	};	
 
 }
