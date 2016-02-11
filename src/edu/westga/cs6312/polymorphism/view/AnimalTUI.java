2ebf3abf390785c1a747f2b2aee7b6c163066f33
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-11 03:46:02 -0500

    # added scanner object and array instance varable to AnimalTUI

--- src/edu/westga/cs6312/polymorphism/view/AnimalTUI.java
+++ src/edu/westga/cs6312/polymorphism/view/AnimalTUI.java
@@ -1,5 +1,10 @@
 package edu.westga.cs6312.polymorphism.view;
 
+import edu.westga.cs6312.polymorphism.model.Animal;
+import com.sun.java_cup.internal.runtime.Scanner;
+import java.util.ArrayList;
+
+
 /**
  * Homework HW05  
  * @author 	cs6312
@@ -7,8 +12,9 @@ package edu.westga.cs6312.polymorphism.view;
  * @version	02.10.2016
  */
 public class AnimalTUI {
-
 	
+	private Scanner userKeyboard;
+	private ArrayList<Animal> animals;
 
 	/**
 	 * controls program flow
